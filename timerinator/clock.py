caef7a8e8cde116ccc5694d2c9aa13d0f0112ece
Bob King <bob@someotherdude.com>
2019-02-27 14:42:03 -0500

    Add start_time attribute to object. Add Clock.tick_clock()

--- timerinator/clock.py
+++ timerinator/clock.py
@@ -1,12 +1,36 @@
-import datetime
+import datetime as dt
 import multiprocessing
 
 class Clock:
 
-    def __init__(self, name = "New Clock", total_time = 0, current_time = 0, remaining_time = 0, status = "stopped", pid = None):
+    def __init__(self,
+                name = "New Clock",
+                total_time = 0,
+                current_time = 0,
+                remaining_time = 0,
+                start_time = None,
+                status = "stopped",
+                pid = None):
+
         self.name = name
         self.total_time = total_time
         self.current_time = current_time
         self.remaining_time = remaining_time
+        self.start_time = start_time
         self.status = status
         self.pid = pid
+
+    def update_remaining_time(self):
+        if self.current_time > 0:
+            self.remaining_time = self.current_time
+        else:
+            self.remaining_time = 0
+
+    def projected_end_time(self):
+        p_e_t = dt.datetime.now() + dt.timedelta(seconds = self.remaining_time)
+        return p_e_t
+
+    def tick_clock(self):
+        now = dt.datetime.now()
+        self.current_time = int((now - self.start_time).total_seconds())
+        self.update_remaining_time()
--- timerinator/tests/test_clock.py
+++ timerinator/tests/test_clock.py
@@ -1,4 +1,10 @@
+import datetime as dt
 import clock
+
+time_1 = dt.datetime(year = 2019, month = 1, day = 1, hour = 1, minute = 10, second = 10)
+time_2 = dt.datetime(year = 2019, month = 1, day = 1, hour = 1, minute = 10, second = 20)
+time_3 = dt.datetime(year = 2019, month = 1, day = 1, hour = 1, minute = 10, second = 30)
+
 
 def test_make_a_new_clock():
     c = clock.Clock()
@@ -9,14 +15,22 @@ def test_new_clock_with_no_params():
     assert c.name == "New Clock"
     assert c.total_time == 0
     assert c.remaining_time == 0
+    assert c.start_time == None
     assert c.status == "stopped"
     assert c.pid == None
 
 def test_new_clock_with_params():
-        c = clock.Clock(name = "My Clock", total_time = 60, remaining_time = 30, status = "paused", pid = 1)
+        c = clock.Clock(name = "My Clock",
+                        total_time = 60,
+                        remaining_time = 30,
+                        start_time = time_1,
+                        status = "paused",
+                        pid = 1)
+
         assert c.name == "My Clock"
         assert c.total_time == 60
         assert c.remaining_time == 30
+        assert c.start_time == time_1
         assert c.status == "paused"
         assert c.pid == 1
 
@@ -40,6 +54,11 @@ def test_change_clock_remaining_time():
     c.remaining_time = 99
     assert c.remaining_time == 99
 
+def test_change_clock_start_time():
+    c = clock.Clock()
+    c.start_time = time_2
+    assert c.start_time == time_2
+
 def test_change_clock_status():
     c = clock.Clock()
     c.status = "purple"
@@ -49,3 +68,35 @@ def test_change_clock_pid():
     c = clock.Clock()
     c.pid = 99
     assert c.pid == 99
+
+def test_update_remaining_time_is_1():
+    c = clock.Clock()
+    c.current_time = 1
+    c.update_remaining_time()
+    assert c.remaining_time == 1
+
+def test_update_remaining_time_is_0():
+    c = clock.Clock()
+    c.current_time = 0
+    c.update_remaining_time()
+    assert c.remaining_time == 0
+
+def test_update_remaining_time_is_neg_1():
+    c = clock.Clock()
+    c.current_time = -1
+    c.update_remaining_time()
+    assert c.remaining_time == 0
+
+def test_projected_end_time():
+    c = clock.Clock()
+    c.remaining_time = 30
+    assert c.projected_end_time().strftime("%M:%S") == (dt.datetime.now() + dt.timedelta(seconds =c.remaining_time)).strftime("%M:%S")
+
+def test_tick_clock():
+    c = clock.Clock(start_time = dt.datetime.now() - dt.timedelta(seconds = 15),
+                    total_time = 60,
+                    current_time = 44,
+                    remaining_time = 44)
+    c.tick_clock()
+    assert c.current_time == 15
+    assert c.remaining_time == 15
